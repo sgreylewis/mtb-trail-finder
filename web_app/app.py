43208f90ce608cef23cf0dc1629d45da6a5a61a8
Sarah Lewis <sgreylewis@gmail.com>
2018-01-24 14:54:55 -0700

    app.py update

--- web_app/app.py
+++ web_app/app.py
@@ -236,4 +236,4 @@ def get_trail_names(): #this makes the dropdown for the trails
         return 'You must enter a state and location.'
 
 if  __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=3000, debug=True, threaded=True)
